de7c45090ab993df597a573a8500b0f89ff1798d
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2025-08-09 17:40:29 +0900

    Add: Click counter component

---
+++ src/components/ClickCounter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>{{ title }}</h2>
+    <button @click="count++">カウント: {{ count }}</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      count: 0,
+      title: 'カウンター',
+    }
+  },
+}
+</script>
